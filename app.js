9ec53bf1ccbe6d622b376bb01bb4e6a32e647cf9
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-07-11 02:50:41 +0600

    added palyer image

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-const loadALLPlayer = () =>
-{
-    fetch('https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    });
-}
+const loadALLPlayer = (playerName) =>
+    {
+        fetch(`https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${playerName}`)
+        .then(res => res.json())
+        .then(data => {
+            displayPlayer(data.player);
+        })
+        .catch(error => {
+            console.error('Error Fetching data:', error);
+            displayPlayer([]);
+        });
+    };
+    
+const displayPlayer = (players) =>
+    {
+        const playerContainer = document.getElementById("player-container");
+        playerContainer.innerHTML = '';
+    
+        players.forEach((player) =>
+        {
+            const div = document.createElement("div");
+            div.classList.add("card");
+            div.dataset.playerId = player.idPlayer;
+            const playerImage = player.strThumb ? player.strThumb : 'unknown.jpg';
+            div.innerHTML = `
+                <img class = "card-img" src=${playerImage} alt = "${player.strPlayer}" />
+                <h3>${player.strPlayer}</h3>
+            `;
+            playerContainer.appendChild(div);
+        });
+    };
 
-loadALLPlayer()
+loadALLPlayer('')
