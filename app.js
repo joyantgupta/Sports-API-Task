f3684aa8ae18e762cafb0f2d9b1ae73e64e9f0e9
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-07-11 04:47:20 +0600

    adding linebar

--- app.js
+++ app.js
@@ -15,7 +15,18 @@ const displayPlayer = (players) =>
     {
         const playerContainer = document.getElementById("player-container");
         playerContainer.innerHTML = '';
-    
+
+        if(players == null || players.length == 0)
+        {
+            const div = document.createElement("div");
+            div.classList.add("comment");
+            div.innerHTML = `
+                <h1>No players found</h1>
+            `
+            playerContainer.appendChild(div);
+            return;
+        }
+
         players.forEach((player) =>
         {
             const div = document.createElement("div");
@@ -25,9 +36,48 @@ const displayPlayer = (players) =>
             div.innerHTML = `
                 <img class = "card-img" src=${playerImage} alt = "${player.strPlayer}" />
                 <h3>${player.strPlayer}</h3>
+                <div class="first-line">
+                    <div class="line-item">
+                        <h6 class="nation-one">Nationality</h6>
+                        <h6 class="nation-two">${player.strNationality}</h6>
+                    </div>
+                    <div class="line-item">
+                        <h6 class="nation-one">Team</h6>
+                        <h6 class="nation-two">${player.strTeam}</h6>
+                    </div>
+                    <div class="line-item">
+                        <h6 class="nation-one">Sport</h6>
+                        <h6 class="nation-two">${player.strSport}</h6>
+                    </div>
+                </div>
+                
             `;
             playerContainer.appendChild(div);
         });
     };
+
+document.getElementById("srch").addEventListener("click", () =>
+{
+    const playerContainer = document.getElementById("player-container");
+    playerContainer.innerHTML = '';
+
+    const inputValue = document.getElementById("srch-box").value.trim();
+    if(inputValue)
+    {
+        loadALLPlayer(inputValue)
+    }
+    else
+    {
+        const div = document.createElement("div");
+        div.classList.add("no-srch");
+        div.innerHTML = `
+            <h2>Please enter a search item</h2>
+        `
+        playerContainer.appendChild(div);
+        return;
+    }
+    document.getElementById("srch-box").value = "";
+}
+)
 
 loadALLPlayer('')
